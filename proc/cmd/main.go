fa9ee5666ec37112a25db35482fa5267452364b5
Marko Vidaković <markovidakovic29@gmail.com>
2025-03-29 22:08:14 +0100

    proc service signaling

--- proc/cmd/main.go
+++ proc/cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("hello from processing servics")
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	fmt.Println("processing service listening for messages")
+	<-sigCh
+	fmt.Println("processing service shutting down")
 }
